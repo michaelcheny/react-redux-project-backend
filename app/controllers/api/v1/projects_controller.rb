e743e675ed63a55419af7d78a32e81f38b550877
Michael Chen <michaelchengaming@gmail.com>
2020-02-22 01:32:54 -0800

    add index create show for projects controller

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,2 +1,35 @@
-class ProjectsController < ApplicationController
+class Api::V1::ProjectsController < ApplicationController
+
+  def index
+    if params[:user_id]
+      projects = User.find(params[:user_id]).projects
+    else
+      projects = Project.all
+    end
+    render json: projects, include: [:users => {except: [:created_at, :updated_at]}], status: 200
+  end
+
+  def create
+    user = current_user
+    project = user.projects.build(project_params)
+    if project.save
+      render json: project, include: [:users], status: 200
+    else
+      render json: { messae: "There was an error creating this project" }, status: 400
+    end
+  end
+
+  def show
+    project = Project.find(params[:id])
+    render json: project
+  end
+
+  sdfaksfasjfajkslhfsdjkcontinue here
+  
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+  
 end
