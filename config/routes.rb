526e8432d5b7a137f4bbc95d340b14d976820c6f
Michael Chen <michaelchengaming@gmail.com>
2020-02-22 00:48:29 -0800

    move routes into the namespace api block

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :reactions
-  resources :comments
-  resources :user_projects
-  resources :projects
   namespace :api do
     namespace :v1 do
-
-      resources :users
       
-      # Routes for Google authentication
-
+      resources :users
+      resources :reactions
+      resources :comments
+      resources :user_projects
+      resources :projects
+      
     end
   end
-
+  
   post 'signup' => 'api/v1/users#create'
-
+  
   get 'auth-check' => 'api/v1/sessions#auth_check'
   # get 'auth/:provider/callback' => 'sessions#googleAuth'
   # get 'auth/failure' => redirect('/')
+
+  # Routes for Google authentication
   get 'auth/:provider/callback', to: 'api/v1/sessions#googleAuth'
   get 'auth/failure', to: redirect('/')
   # get '/signup' => 'accounts#new', as: 'signup'
