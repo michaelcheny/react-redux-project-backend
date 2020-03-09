00c9d9e70aac59f9697e615bb3320b7c06253311
Michael Chen <michaelchengaming@gmail.com>
2020-03-08 17:39:34 -0700

    add except created at and updated at for categories

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::CategoriesController < ApplicationController
 
   def index
     category = Category.all
-    render json: category, include: [:projects => {include: :users}]
+    render json: category, include: [:projects => {include: :users}], except: [:created_at, :updated_at]
   end
 
   def show
     category = Category.find_by(id: params[:id])
-    render json: category, include: [:projects => {include: :users}]
+    render json: category, include: [:projects => {include: :users}], except: [:created_at, :updated_at]
   end
   
 end
