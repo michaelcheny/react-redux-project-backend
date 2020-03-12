4ff480f6bdcf353c631488c81b495b0c504359c7
Michael Chen <michaelchengaming@gmail.com>
2020-03-12 03:17:44 -0700

    fix comment controller to have strong comment params

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,9 +3,18 @@ class Api::V1::CommentsController < ApplicationController
 
   def create
     user = current_user
-    project = Project.find_by(id: params[:project])
-    comment = Comment.create(user_id: user.id, commentable: project, content: params[:content])
+    project = Project.find_by(id: params[:projectId])
+    # comment = Comment.create(user_id: user.id, commentable: project, content: comment_params)
+    comment = Comment.create(comment_params)
+    # binding.pry
+    comment.update(user: user, commentable: project)
     render json: comment
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
   
 end
