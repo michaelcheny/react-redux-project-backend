6e975e9155c4bd2f9ab0f9db57381a62d784f7b2
Michael Chen <michaelchengaming@gmail.com>
2020-03-18 02:57:27 -0700

    user projects have the correct api flag and correctly renders project at creation

--- app/controllers/api/v1/user_projects_controller.rb
+++ app/controllers/api/v1/user_projects_controller.rb
@@ -1,11 +1,11 @@
-class UserProjectsController < ApplicationController
+class Api::V1::UserProjectsController < ApplicationController
 
   def create
     user = current_user
-    project = Project.find_by(id: params[:project][:id])
+    project = Project.find_by(id: params[:projectId])
     up = user.user_projects.build(project: project)
     if up.save
-      render json: up, status: 200
+      render json: project, include: [:users], status: 200
     else
       render json: { message: 'There was an issue.'}, status: 400
     end
