2d32113a8fdfefd43de347945012151fdff84394
Michael Chen <michaelchengaming@gmail.com>
2020-03-20 12:22:29 -0700

    sort comments order by created at

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  default_scope { order(created_at: :asc) }
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable, dependent: :destroy
